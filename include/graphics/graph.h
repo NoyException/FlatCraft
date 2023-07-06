55232fa593cce90fe50e12338d1958de18791b2c
朝 唐 <tangdynasty0@outlook.com>
2023-07-06 15:49:08 +0800

    graph

--- include/graphics/graph.h
+++ include/graphics/graph.h
@@ -5,14 +5,24 @@
 #include "SDL.h"
 #include "SDL_image.h"
 extern class DestroyBlock destroyBlock;
+extern class BlockSurface blockSurface;
 void graphMain(FlatCraft* game);
 
-class BlockGraph {//search the image through material
+class BlockSurface {//store all of the texture
 public:
-	BlockGraph();
-//	inline IMAGE* getImage(Material material) {return nullptr;}
+	BlockSurface() {
+		SDL_Surface* pic = nullptr;
+		pic = IMG_Load("../../../../src/graphics/textures/block/stone.png");
+		surfaces[Material::STONE] = pic;
+
+	}
+	inline SDL_Surface* getSurface(Material material) {
+		if (surfaces.find(material) == surfaces.end())
+			return surfaces[Material::STONE];
+		return surfaces[material];
+	}
 private:
-//	std::vector<IMAGE> imgs;
+	std::unordered_map<Material, SDL_Surface*> surfaces;
 };
 
 class DestroyBlock {
@@ -31,13 +41,17 @@ private:
 	int progress;//the progress of destroying the block, from 0 to 100
 };
 
+
 class Graph {
 public:
-	Graph(FlatCraft* game) : game(game) {}
+	Graph(FlatCraft* game) : game(game), windowWidth(1280), windowHeight(768), blockSize(32) {}
 	void display();//display the graph, including world and player
-	void drawMap(Location location);//draw the map as location as the center
+	void drawMap(Location location, SDL_Renderer* renderer);//draw the map as location as the center
+	void draw(SDL_Renderer* renderer);//draw the graph on the renderer
 private:
 	FlatCraft* game;
+	int windowWidth, windowHeight;//pixel
+	int blockSize;//pixel
 };
 
 #endif
--- src/graphics/graph.cpp
+++ src/graphics/graph.cpp
@@ -1,12 +1,9 @@
-
+Ôªø
 #include "graphics/graph.h"
 #include <chrono>
 DestroyBlock destroyBlock;
+BlockSurface blockSurface;
 void graphMain(FlatCraft *game) {
-	//int rW = GetSystemMetrics(SM_CXSCREEN); // ∆¡ƒªøÌ∂» œÒÀÿ
-	//int rH = GetSystemMetrics(SM_CYSCREEN); // ∆¡ƒª∏ﬂ∂» œÒÀÿ
-	//initgraph(rW, rH);
-//	initgraph(1280, 768);
 	Graph graph(game);
 	graph.display();
 	
@@ -14,37 +11,35 @@ void graphMain(FlatCraft *game) {
 void Graph::display() {
 	//World* world = game->getPlayer()->getLocation().getWorld();
 	//World* world = FlatCraft::getInstance()->getWorld("test");
-    const char* path = "../../../../src/graphics/textures/birch.bmp";
-
+    //const char* path = "../../../../src/graphics/textures/birch.bmp";
 	SDL_Init(SDL_INIT_EVERYTHING);
-	SDL_Rect rect;
-	rect.x = 100;
-	rect.y = 400;
-	rect.w = 32;
-	rect.h = 32;
 	SDL_Surface* pic = nullptr, *screen = nullptr;
-	SDL_Window *window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 768, SDL_WINDOW_SHOWN);
+	SDL_Window *window = SDL_CreateWindow("FlatCraft", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, windowWidth, windowHeight, SDL_WINDOW_SHOWN);//create window
 	//pic = SDL_LoadBMP(path);
-	pic = IMG_Load(path);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, pic);
 	while (true) {
 		std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
 		std::chrono::milliseconds duration(50);
 		std::chrono::system_clock::time_point targetTime = now + duration;
-		std::this_thread::sleep_until(targetTime);
-		rect.x += 4;
-		SDL_RenderClear(renderer); // «Â≥˝‰÷»æ∆˜
-		SDL_RenderCopy(renderer, texture, NULL, &rect); // øΩ±¥Œ∆¿ÌµΩ‰÷»æ∆˜
-		SDL_RenderPresent(renderer); // ‰÷»æ
+		std::this_thread::sleep_until(targetTime);//wait
+		SDL_RenderClear(renderer); //clear before image in renderer
+		draw(renderer);
+		//SDL_RenderCopy(renderer, texture, NULL, &rect); 
+		SDL_RenderPresent(renderer); //output image
 	}
-	//SDL_BlitSurface(pic, NULL, screen, NULL);
-	//SDL_Rect rect;
-	//rect.x = 100;
-	//rect.y = 400;
-	//rect.w = 10;
-	//rect.h = 5;
-	//SDL_BlitSurface(pic, NULL, screen, &rect);
+}
+void Graph::draw(SDL_Renderer* renderer) {
+	Location location("test", 64, 0);
+	drawMap(location , renderer);
 
-	//SDL_UpdateWindowSurface(window);
+}
+
+void Graph::drawMap(Location location, SDL_Renderer* renderer) {
+	SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, blockSurface.getSurface(Material::STONE));
+	SDL_Rect rect;
+	rect.x = 20;
+	rect.y = 20;
+	rect.w = rect.h = blockSize;
+	SDL_RenderCopy(renderer, texture, NULL, &rect);
+
 }
